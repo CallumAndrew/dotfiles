c87489a24325f35c296ad2a4b136734aab54f280
Callum Andrew <sharkytheshark@mailbox.org>
2022-07-12 10:53:00 +1000

    Simplify nvim and tmux

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- Settings
-vim.wo.nu = true -- Absolute line numbers
-
--- Plugins
-return require('packer').startup(function()
-	use 'wbthomason/packer.nvim' -- Packer
-	use 'tpope/vim-fugitive' -- Git commands
-end)
---
+++ .config/nvim/init.vim
@@ -0,0 +1,2 @@
+" Absolute line numbers
+set number
